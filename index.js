441fe3a175f656c09256660a4ddffab4d4145b09
IngJulian <juliaaan7829@gmail.com>
2026-06-03 23:46:07 -0500

    feature:  se agregan rutas basicas CRUD productos

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //Instalamos express
 import express from 'express';
+
+//Base de datos provisional
+import db from './db.json' with {type: 'json'};
 //Definiciones iniciales
 const PORT = process.env.PORT ?? '3000';
 const app = express();
@@ -15,6 +18,57 @@ app.get('/health' , (req, res) => {
         status: "ok" , 
         uptime: process.uptime()
     })
+})
+
+
+/*Routes*/
+/*Catalogo*/ 
+app.get('/catalogo' , (req , res) => { //Obtener todos
+    res.json({db});
+})
+app.get('/catalogo/:id' , (req , res ) => {//Obtener uno
+    const {id} = req.params;
+    console.log(id);
+    const resultado = db.find(perfume => perfume.id == id );
+    res.json({resultado});
+});
+app.post('/catalogo' , (req,res) => { //Registrar
+    const {id , nombre , precio , imagen} = req.body;
+    
+    const perfume = {
+        id ,
+        nombre ,
+        precio , 
+        imagen
+    }
+    
+    db.push(perfume);
+    res.json({message: "Registrado correctamente"});
+})
+
+app.put('/catalogo/:id' , (req, res) => {
+    const {id} = req.params;
+    const {nombre , precio , imagen } = req.body;
+
+    const nuevoPerfume = {
+        nombre ,
+        precio ,
+        imagen 
+    }
+
+    const indice = db.findIndex(perfume => perfume.id == id);
+    db[indice] = nuevoPerfume;
+    res.json({message: "Actualizado correctamente"})
+})
+
+
+app.delete('/catalogo/:id' , (req , res) => { //Eliminar por Id
+    const {id} = req.params;
+    const indice = db.findIndex((perfume , index) => perfume.id == id );
+
+    db.splice(indice,1);
+    res.json({message: "Eliminado con exito"})
+
 })
 
 
